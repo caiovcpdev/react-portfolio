42eaa774e23b461f7b41660b0210cb3cdcbfe6a3
caiovcpdev <caiov761@gmail.com>
2023-04-10 20:43:18 -0300

    :/

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import {BsFillMoonStarsFill} from 'react-icons/bs'
 import {AiFillLinkedin} from 'react-icons/ai'
 import Image from "next/image"
@@ -12,13 +13,16 @@ import web4 from "../../public/web4.png";
 import web5 from "../../public/web5.png";
 import web6 from "../../public/web6.png";
 export default function Home() {
+  const [darkMode, setDarkMode] = useState(false);
+
   return (
+    <div className={darkMode ? "dark":''}>
     <main className='bg-white px-10 md:px-20 lg:px-40'>
       <section className="min-h-screen"> 
         <nav className='py-10 mb-12 flex justify-between'>
           <h1 className="font-burtons text-xl">developedbyed</h1>
           <ul className='flex items-center'>
-            <li><BsFillMoonStarsFill className='cursor-pointer text-2xl'/></li>
+            <li><BsFillMoonStarsFill  onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-2xl'/></li>
             <li><a className=' bg-gradient-to-r  from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Resume</a></li>
           </ul>
         </nav>
@@ -159,5 +163,6 @@ export default function Home() {
           </div>
       </section>
     </main>
+    </div>  
   )
 }
