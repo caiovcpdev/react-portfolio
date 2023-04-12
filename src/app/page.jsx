badc477c45be6d44f25b3700519ceadbf44b2810
caiovcpdev <caiov761@gmail.com>
2023-04-12 20:35:31 -0300

    Its donegit add .

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,15 +3,13 @@
 import {BsFillMoonStarsFill} from 'react-icons/bs'
 import {AiFillLinkedin} from 'react-icons/ai'
 import Image from "next/image"
-import deved from "../../public/dev-ed-wave.png"
+import deved from "../../public/robot.png"
 import design from "../../public/design.png"
 import code from "../../public/code.png";
 import web1 from "../../public/Client-Portfolio.jpeg";
 import web2 from "../../public/iFacturing.jpeg";
 import web3 from "../../public/Spotify-Clone.jpeg";
 import web4 from "../../public/E-Commerce.jpeg";
-import web5 from "../../public/web5.png";
-import web6 from "../../public/web6.png";
 import { useState } from 'react'
 
 
@@ -25,7 +23,7 @@ const [darkMode, setDarkMode] = useState(false);
           <h1 className="font-burtons text-xl">caiovcpdev</h1>
           <ul className='flex items-center'>
             <li><BsFillMoonStarsFill onClick={() => setDarkMode(!darkMode)} className='cursor-pointer text-2xl'/></li>
-            <li><a className=' bg-gradient-to-r  from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Resume</a></li>
+            <li><a className=' bg-gradient-to-r  from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="https://www.linkedin.com/in/caio-vinicius-59636523a/">Resume</a></li>
           </ul>
         </nav>
         <div className='text-center p-10'>
@@ -34,7 +32,9 @@ const [darkMode, setDarkMode] = useState(false);
           <p className='text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto dark:text-white'>Como desenvolvedor, meu objetivo principal é criar soluções de software para resolver problemas ou atender a necessidades de negócios. Isso geralmente envolve escrever código em uma ou mais linguagens de programação, depurar problemas de software e trabalhar com outros desenvolvedores para integrar o código em sistemas maiores.</p>
         </div>
         <div className='text-5xl flex justify-center py-3 text-gray-600 cursor-pointer'>
+          <a href="https://www.linkedin.com/in/caio-vinicius-59636523a/">
           <AiFillLinkedin/>
+          </a>
         </div>
         <div className='relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96'>
           <Image src={deved} fill style={{objectFit:"cover"}}/>
@@ -85,36 +85,44 @@ const [darkMode, setDarkMode] = useState(false);
           </div>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
           <div className="basis-1/3 flex-1 ">
+            <a href="https://client-portfolio-ten.vercel.app">
               <Image
                 className="rounded-lg object-cover"
                 width={"100%"}
                 height={"100%"}
                 src={web1}
               />
+              </a>
             </div>
             <div className="basis-1/3 flex-1">
+              <a href="https://ass-factoring-front-end.vercel.app">
               <Image
                 className="rounded-lg object-cover"
                 width={"100%"}
                 height={"100%"}
                 src={web2}
               />
+              </a>
             </div>
             <div className="basis-1/3 flex-1">
+              <a href="https://spotify-clone-blush-eta.vercel.app">
               <Image
                 className="rounded-lg object-cover"
                 width={"100%"}
                 height={"100%"}
                 src={web3}
               />
+              </a>
             </div>
             <div className="basis-1/3 flex-1">
+              <a href="https://e-commerce-website-sigma.vercel.app">
               <Image
                 className="rounded-lg object-cover"
                 width={"100%"}
                 height={"100%"}
                 src={web4}
               />
+              </a>
             </div>
           </div>
       </section>
